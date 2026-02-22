d0a7e24016831ed25fefc7f779519cec43c64a9f
Nemida <144930141+Nemida@users.noreply.github.com>
2026-02-22 17:58:14 +0530

    feat: styled search bar

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import "./searchBar.scss";
 
+const types = ["buy", "rent"];
+
 const SearchBar = () => {
+    const [query, setQuery] = useState({
+        type: "buy",
+        location: "",
+        minPrice: 0,
+        maxPrice: 0,
+    });
+
+    const switchType = (val) => {
+        setQuery((prev) => ({
+            ...prev,
+            type: val,
+        }));
+    };
     return (
         <div className="searchBar">
-            <div className="type"></div>
+            <div className="type">
+                {types.map((type) => (
+                    <button
+                        key={type}
+                        onClick={() => switchType(type)}
+                        className={query.type === type ? "active" : ""}
+                    >
+                        {type}
+                    </button>
+                ))}
+            </div>
             <form action="">
                 <input
                     type="text"
@@ -24,6 +50,9 @@ const SearchBar = () => {
                     max={100000}
                     placeholder="Max Price"
                 />
+                <button>
+                    <img src="/search.png" alt="" />
+                </button>
             </form>
         </div>
     );
