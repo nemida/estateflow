894f701540dda5c49c94b2be41d3bd0c4b11e2aa
Nemida <144930141+Nemida@users.noreply.github.com>
2026-04-06 14:03:45 +0530

    feat: added Card and Filter component

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,7 +1,23 @@
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
+import { listData } from "../../lib/dummyData";
 import "./listPage.scss";
 
 const ListPage = () => {
-    return <div className="listPage">ListPage</div>;
+    const data = listData;
+    return (
+        <div className="listPage">
+            <div className="listContainer">
+                <div className="wrapper">
+                    <Filter />
+                    {data.map((item) => (
+                        <Card key={item.id} item={item} />
+                    ))}
+                </div>
+            </div>
+            <div className="mapContainer">Map</div>
+        </div>
+    );
 };
 
 export default ListPage;
